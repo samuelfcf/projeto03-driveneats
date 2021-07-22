7354c2d7976bc97a71137dce3d0712d358c2a3cf
Samuel Felipe <samuelfelipef@gmail.com>
2021-07-22 13:57:47 -0300

    rename functions

--- script.js
+++ script.js
@@ -3,32 +3,32 @@ const opcoesBebidas = document.querySelectorAll(".bebida");
 const opcoesSobremesas = document.querySelectorAll(".sobremesa");
 
 
-function selecionaOpcao(click_id, className) {
+function selectOption(click_id, className) {
   
   if (className === "opcao prato") {
-    retiraBorda(opcoesPratos);
-    addBorda(click_id); 
+    removeSelectedStyle(opcoesPratos);
+    addSelectedStyle(click_id); 
 
   } else if (className === "opcao bebida") {
-    retiraBorda(opcoesBebidas);
-    addBorda(click_id); 
+    removeSelectedStyle(opcoesBebidas);
+    addSelectedStyle(click_id); 
 
   } else if (className === "opcao sobremesa") {
-    retiraBorda(opcoesSobremesas);
-    addBorda(click_id); 
+    removeSelectedStyle(opcoesSobremesas);
+    addSelectedStyle(click_id); 
 
   }
   
 }
 
-function retiraBorda(elements) {
+function removeSelectedStyle(elements) {
   elements.forEach(opcao => {
     opcao.style.border = "none";
     opcao.children[3].children[1].style.display = "none";
   });
 }
 
-function addBorda(id) {
+function addSelectedStyle(id) {
   const element = document.getElementById(id);
   const icon = element.children[3].children[1];
   element.style.border = "2px solid #32B72F";
