d97e926d24313f41c3537d633ae38664283d701b
Samuel Felipe <samuelfelipef@gmail.com>
2021-07-22 14:49:03 -0300

    add checkout button feature

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+const opcoes = document.querySelectorAll(".opcoes");
 const opcoesPratos = document.querySelectorAll(".prato");
 const opcoesBebidas = document.querySelectorAll(".bebida");
 const opcoesSobremesas = document.querySelectorAll(".sobremesa");
+
+const button = document.querySelector("button");;
 
 
 function selectOption(click_id, className) {
@@ -8,16 +11,20 @@ function selectOption(click_id, className) {
   if (className === "opcao prato") {
     removeSelectedStyle(opcoesPratos);
     addSelectedStyle(click_id); 
+    opcoes[0].classList.add("selected")
 
   } else if (className === "opcao bebida") {
     removeSelectedStyle(opcoesBebidas);
     addSelectedStyle(click_id); 
+    opcoes[1].classList.add("selected")
 
   } else if (className === "opcao sobremesa") {
     removeSelectedStyle(opcoesSobremesas);
     addSelectedStyle(click_id); 
-
+    opcoes[2].classList.add("selected")
   }
+
+  enableButtom(opcoes);
   
 }
 
@@ -34,3 +41,24 @@ function addSelectedStyle(id) {
   element.style.border = "2px solid #32B72F";
   icon.style.display = "initial";
 }
+
+function enableButtom(opcoes) {
+
+  let cont = 0;
+
+  opcoes.forEach(opcao => {
+    if (opcao.classList.contains("selected")) {
+      cont++;
+    }
+  });
+
+  if(cont === 3) {
+    button.disabled = false;
+    button.innerHTML = "Fechar pedido";
+    button.style.backgroundColor = "#32B72F";
+  }
+}
+
+
+
+
